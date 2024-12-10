2002412e0c363d0b752465272b29c4081d7e7d06
karthikponna <karthikponna963@gmail.com>
2024-12-10 13:29:52 +0000

    updated the code

---
+++ run_ci_cd.py
@@ -0,0 +1,6 @@
+from pipelines.training_pipeline import ml_pipeline
+
+if __name__ == "__main__":
+    # Run the ML pipeline
+    run = ml_pipeline()
+    print("Pipeline run completed successfully.")
